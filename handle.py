10e356c5b5fdec15d79c7a03e345c944083c5d57
Soybeansprit <1284541004@qq.com>
2020-06-09 19:43:42 +0800

    Update handle.py

--- handle.py
+++ handle.py
@@ -8,16 +8,16 @@ from getData import *
 class Handle(object):
     def POST(self):
         try:
-            webData = web.data()
+            webData = web.data()     #数据获取？？
             print "Handle Post webdata is ", webData   #后台打日志
-            recMsg = receive.parse_xml(webData)
+            recMsg = receive.parse_xml(webData)      #难道是返回了self？
             if isinstance(recMsg, receive.Msg):
                 toUser = recMsg.FromUserName
                 fromUser = recMsg.ToUserName
-                if recMsg.MsgType == 'text':
-                	if recMsg.Content == '温度':
+                if recMsg.MsgType == 'text':     
+                	if recMsg.Content == '温度':    #获取温度：时间\n室内温度为xx℃
 				content = str(getData_time('temperature'))+'\n室内温度为'+str(getData_value('temperature')) +'℃'
-		  	elif recMsg.Content == '湿度':
+		  	elif recMsg.Content == '湿度':    #获取湿度：时间\n室内湿度为xx℃
 				content = str(getData_time('humidity'))+'\n室内湿度为'+str(getData_value('humidity')) +'%'
 			else:
 				content = '抱歉尚未开通这项指令功能，你可以尝试发送‘温度’、‘湿度’来查看最新的室内信息,或者发送相应的语音消息 '
@@ -30,8 +30,8 @@ class Handle(object):
 				content = str(getData_time('humidity'))+'\n室内湿度为'+str(getData_value('humidity')) +'%'
 			else:
 				content =recMsg.Recognition+'\n无法识别这条语音消息'
-                    	replyMsg = reply.TextMsg(toUser, fromUser, content)
-                    	return replyMsg.send()
+                    	replyMsg = reply.TextMsg(toUser, fromUser, content)    #获取xml种对应消息的各内容
+                    	return replyMsg.send()                       #消息格式转换，转换为xml
                 else:
                     	return reply.Msg().send()
             else:
